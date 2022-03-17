fa44126faf643722fa41810664a7509f204b9272
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-17 20:08:04 +0600

    get currently logged in user profile with checking Authenticated

--- controller/authController.js
+++ controller/authController.js
@@ -127,3 +127,13 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
 
   sendToken(user, 200, res);
 });
+
+// Get currently logged in user details   =>   /api/v1/me
+exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
--- routes/auth.js
+++ routes/auth.js
@@ -6,11 +6,17 @@ const {
   loginUser,
   forgotPassword,
   resetPassword,
+  getUserProfile,
 } = require("../controller/authController");
+const { isAuthenticatedUser } = require("../middleware/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+
+// protected routes => Unauthenicated user can not access
+
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
 
 module.exports = router;
