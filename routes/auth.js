381d7a0ed730d4d917734b71f4af8706d52c876f
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-17 11:58:23 +0600

    implement user register & login

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser } = require("../controller/authController");
+const { registerUser, loginUser } = require("../controller/authController");
 
 router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
 
 module.exports = router;
