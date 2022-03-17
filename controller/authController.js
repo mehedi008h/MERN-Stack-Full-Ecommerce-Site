4677b8c233c8dfcd034fd13b1646346918477a78
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-17 20:21:58 +0600

    update profile & logout

--- controller/authController.js
+++ controller/authController.js
@@ -171,3 +171,16 @@ exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
     success: true,
   });
 });
+
+// Logout user   =>   /api/v1/logout
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logged out",
+  });
+});
--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,7 @@ const {
   getUserProfile,
   updatePassword,
   updateProfile,
+  logout,
 } = require("../controller/authController");
 const { isAuthenticatedUser } = require("../middleware/auth");
 
@@ -16,6 +17,7 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+router.route("/logout").get(logout);
 
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
