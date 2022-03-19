eea7a9eb27ff46acbe62cab0f3f72c9662b1b6e4
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-19 19:54:26 +0600

    design login page

--- client/src/pages/auth/login/Login.js
+++ client/src/pages/auth/login/Login.js
@@ -1,12 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Login.module.scss";
 
 const Login = () => {
-  return (
-    <div className={styles.login}>
-      <h1>Login</h1>
-    </div>
-  );
+    return (
+        <div className={styles.login}>
+            <div className={styles.login_container}>
+                <h3 className="text-center text-white mb-3">Login</h3>
+                <div className={styles.from_group}>
+                    <label htmlFor="email_field">Email</label>
+                    <input type="email" placeholder="Enter your email ..." />
+                </div>
+                <div className={styles.from_group}>
+                    <label htmlFor="password_field">Password</label>
+                    <input
+                        type="password"
+                        placeholder="Enter your password ..."
+                    />
+                </div>
+                <div className={styles.from_group}>
+                    <button>Login</button>
+                </div>
+                <div className={styles.from_group}>
+                    <p className="text-center text-white">
+                        Dont Have Account ? <Link to="/signup">Signup</Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
