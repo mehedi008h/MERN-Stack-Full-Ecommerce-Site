f3d192cd2a6af2e805207f710f1ef5d750fac7cf
Mehedi Hasan <mehedi08h@gmail.com>
2023-01-05 21:14:14 +0600

    change url

--- client/src/config.js
+++ client/src/config.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL:
-        "https://mern-stack-full-ecommerce-site-production-c004.up.railway.app",
+        "https://e-shop-app.onrender.com",
 });
